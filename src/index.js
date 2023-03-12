120451f283f105290d2a93bf79f5ad1752a37266
mrdoggy1234 <46692237+mrdoggy1234@users.noreply.github.com>
2023-03-12 01:42:59 -0500

    removed build dir from gitignore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 import * as ReactDOMClient from 'react-dom/client';
@@ -14,6 +15,11 @@ root.render(
   <App />
 </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
